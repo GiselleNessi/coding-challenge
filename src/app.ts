d4b9780bda2fafb04387ca82a8eebcfd4b798bf9
Giselle Chacon Nessi <gisecn@gmail.com>
2022-07-29 13:35:56 +0200

    read csv file content

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,39 @@
 import express from "express";
-import path from "path";
+import * as fs from "fs";
+import * as path from "path";
+import { parse } from "csv-parse";
 
 import { loadApiEndpoints } from "./controllers/api";
+
+type Influencers = {
+    instaName: string;
+    name: string;
+    categoryOne: string;
+    categoryTwo: string;
+    followers: number;
+    country: string;
+    engagement: number;
+    avg: number;
+  };
+  
+  (() => {
+    const csvFilePath = path.resolve(__dirname, 'instagram_influencers.csv');
+  
+    const headers = ['instaName', 'name', 'categoryOne', 'categoryTwo', 'followers', 'country', 'engagement', 'avg'];
+  
+    const fileContent = fs.readFileSync(csvFilePath, { encoding: 'utf-8' });
+  
+    parse(fileContent, {
+      delimiter: ',',
+      columns: headers,
+    }, (error: any, result: Influencers[]) => {
+      if (error) {
+        console.error(error);
+      }
+  
+      console.log("Result", result);
+    });
+  })();
 
 // Create Express server
 const app = express();
@@ -18,3 +50,4 @@ app.use(
 loadApiEndpoints(app);
 
 export default app;
+
